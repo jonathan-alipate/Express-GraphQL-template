4b1a7a265500db2003d257956f0f9f2635eb5336
Jonathan Alipate <j.alipate@gmail.com>
2020-12-08 15:09:36 +1300

    added graphql schema

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
+const MyGraphQLSchema = require('./schema.js');
 
 const app = express()
 
@@ -8,4 +9,8 @@ app.use('/graphql', graphqlHTTP({
     graphiql: true
 }))
 
-app.listen(3000)
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log('server started on port ' + PORT)
+})
